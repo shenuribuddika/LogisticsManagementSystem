0dd8b35bb1998c34c0c2bae8eda8951f1f07ea2b
shenuribuddika <shenuribuddika@gmail.com>
2025-10-22 20:37:31 +0530

    Added main method

--- src/logisticsmanagementsystem/LogisticsManagementSystem.java
+++ src/logisticsmanagementsystem/LogisticsManagementSystem.java
@@ -29,6 +29,48 @@ public class LogisticsManagementSystem {
     
     static class Delivery{}
     
+    
+    public static void main(String[] args) {
+        while (true){
+            System.out.println("------- DELIVERY COST ESTIMATION -------- ");
+            System.out.println("1. City Managment");
+            System.out.println("2. Distance Managment");
+            System.out.println("3. Handle Delivery");
+            System.out.println("4. Report");
+            System.out.println("5. Save and Exit");
+            
+            int choice = sc.nextInt();
+            switch (choice){
+                case 1: City_Managment();
+                break;
+                case 2: Distance_Managment();
+                break;
+                case 3: Handle_Delivery();
+                break;
+                case 4: Report();
+                break;
+                case 5: Save_Data();
+                break;
+                
+            }
+        }
+
+    }
+    static void City_Managment(){
+    }
+    static void Distance_Managment(){
+    }
+    static void Handle_Delivery(){
+    }
+    static void Report(){
+    }
+    static void Save_Data(){
+    }
+    
+    
+
+
+    
 }  
     
     
