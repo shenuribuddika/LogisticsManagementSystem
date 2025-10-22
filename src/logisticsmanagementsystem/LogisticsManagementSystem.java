e3efa9b3a207995a8f339377063687c606bccb24
shenuribuddika <shenuribuddika@gmail.com>
2025-10-22 20:27:40 +0530

    Added Public Class

--- src/logisticsmanagementsystem/LogisticsManagementSystem.java
+++ src/logisticsmanagementsystem/LogisticsManagementSystem.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package logisticsmanagementsystem;
+package logisticsManagementsystem;
 
 import java.util.Scanner;
 
@@ -14,7 +14,7 @@ public class LogisticsManagementSystem {
         
     static String[] cities = new String[Max_Cities];
     static int City_Count = 0;
-    static int[][] Distance = new int[Max_Cities][Max_Cities];
+    static int [][] Distance = new int [Max_Cities][Max_Cities];
         
     static String[] Vehicle_Type = {"Van","Truck","Lorry"};
     static int[] Capacity = {1000,5000,10000};
@@ -22,53 +22,17 @@ public class LogisticsManagementSystem {
     static int[] Avg_Speed  = {60,50,45};
     static int[] Fuel_Efficiency = {12,6,4};
         
-    static String [] Deliveries = new String[Max_Deliveries];
+    static Delivery[] deliveries = new Delivery [50];
     static int Delivery_Count = 0;
     
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        
-        while (true){
-            System.out.println("------- DELIVERY COST ESTIMATION -------- ");
-            System.out.println("1. City Managment");
-            System.out.println("2. Distance Managment");
-            System.out.println("3. Handle Delivery");
-            System.out.println("4. Report");
-            System.out.println("5. Save and Exit");
-            
-            int choice = sc.nextInt();
-            switch (choice){
-                case 1: City_Managment();
-                break;
-                case 2: Distance_Managment();
-                break;
-                case 3: Handle_Delivery();
-                break;
-                case 4: Report();
-                break;
-                case 5: Save_Data();
-                break;
-                
-            }
-        }
-
-    }
-    static void City_Managment(){
-    }
+    static Scanner sc = new Scanner(System.in);
     
-    static void Distance_Managment(){
-    }
+    static class Delivery{}
     
-    static void Handle_Delivery(){
-    }
+}  
     
-    static void Report(){
-    }
-    
-    static void Save_Data(){
-    }
     
     
     
         
-}    
+   
